2e29f273fd16aa560f0c42f8bb85b57130a8cb27
deadmojii <mojtaba2022edition@gmail.com>
2023-01-07 12:43:31 +0330

    create project

---
+++ articles/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from . import models
+
+# Create your views here.
+def articles_list(request):
+    articles = models.Article.objects.all().order_by('date')
+
+    args = {'articles':articles}
+    return render(request, 'articles/articleslist.html',args)
+
+
+def first(request):
+    return render(request, 'articles/first.html')
