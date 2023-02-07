b9453ab5c86ef3f94d51855aac119a105281770d
deadmojii <mojtaba2022edition@gmail.com>
2023-02-07 22:54:30 +0330

    end

--- articles/urls.py
+++ articles/urls.py
@@ -5,5 +5,6 @@ app_name = "articles"
 urlpatterns = [
     path('', views.articles_list, name="list"),
     path('first/', views.first),
+    path('create', views.create_article, name='create'),
     path('<slug>', views.article_detail, name="detail"),
 ]
--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'articles',
+    'articles','accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +121,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
